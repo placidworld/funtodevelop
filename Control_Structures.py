f2b8fdceae32b4a8f4c254db3491fcc737e59b49
placidworld <heart2tranquil@gmail.com>
2020-09-02 11:44:37 -0400

    Add files via upload
    How to use if else effectively

---
+++ Control_Structures.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 10:36:09 2020
+
+@author: heart
+"""
+
+import sys # Used for the sys.exit function
+
+int_con6dition = 5
+
+if int_condition < 6:
+    sys.exit("int_condition must be >= 6")
+else:
+    print("int_condition was >=6 - conditioning")
