2a4a1bc1f18662b9e41e647c4662bae3ff218317
placidworld <heart2tranquil@gmail.com>
2020-09-02 12:16:14 -0400

    Add files via upload
    Two methods to show how to convert a list to string for display

---
+++ Converting_List_To_String.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 12:12:52 2020
+
+@author: heart
+"""
+
+# Method 1
+mylist = ['Python', 'Teradata', 'SQL', 'SAS']
+print(', '.join(mylist))
+
+# Method 2
+print("\n".join(mylist))
