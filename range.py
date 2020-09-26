635d06f3c5c96297ac9f4fbe71ff2484b5e067ed
placidworld <heart2tranquil@gmail.com>
2020-09-25 20:27:41 -0400

    Add files via upload
    Range function

--- range.py
+++ range.py
@@ -22,3 +22,56 @@ for i in range(3):
     
 for i in range(6, 1, -2):
     print(i, end="")
+    
+
+# create a for loop that counts from 0 to 10, and prints odd numbers to the screen
+for i in range(1, 11):
+    if i % 2 != 0:
+        print(i)
+        
+# create a while loop that counts from 0 to 10, and prints odd numbers to the screen
+x = 1
+while x < 11:
+    if x % 2 != 0:
+        print(x)        
+    x += 1
+    
+# create a program with a for loop and a break statement. The program should iterate over characters in 
+# an email address, exit the loop when it reaches @ symbol, and print the part before  @ on one line. 
+for ch in "john.smith@jhu.edu":
+    if ch == "@":
+        break
+    print(ch, end="")
+    
+# create a program with a for loop and a continue statement. The program should iterate over a string of 
+# digits, replace each 0 with x, and print the modified string to the screen
+    
+for digit in "0165031806510":
+    if digit == "0":
+        print("x", end="")
+        continue
+    print(digit, e nd="")
+
+
+### 
+n = 3
+while n > 0:
+    print(n + 1)
+    n -= 1
+else:
+    print(n)
+    
+###
+n = range(4)
+
+for num in n:
+    print(num - 1)
+else:
+    print(num)
+    
+###
+for i in range(0, 6, 3):
+    print(i)
+    
+    
+    
