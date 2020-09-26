a58ae3e56854c64340cd7bcbee2dbf27bab8ac21
placidworld <heart2tranquil@gmail.com>
2020-09-25 22:07:09 -0400

    Add files via upload
    Python method .append() and .insert()

---
+++ append_insert.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 21:40:54 2020
+
+@author: heart
+"""
+
+### Append
+myList = []
+
+for i in range(5):
+    myList.append(i + 1)
+
+print(myList)
+
+
+### insert
+myList = []
+
+for i in range(5):
+    myList.insert(0, i + 1)
+    
+print(myList)
